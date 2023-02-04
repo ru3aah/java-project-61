155d7cf452de94c09c0680da7fa69ebd0257bdd4
ru3aah <ru3aah@gmail.com>
2023-02-04 19:13:46 +0300

    add 0 & 1 choices

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,22 @@
 package hexlet.code;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        String usrName = Cli.greeting();
+
+        Scanner gameNum = new Scanner(System.in);
+
+        System.out.println("Please Enter the game number and press Enter,");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        int choice = gameNum.nextInt();
+        if (choice == 1) {
+            System.out.println("Welcome to the Brain Games!");
+            String usrName = Cli.greeting();
+        }
+        else {
+            System.out.println("Bye!");
+        }
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,10 +1,10 @@
 package hexlet.code;
 import java.util.Scanner;
+
 public class Cli {
 
     public static String greeting() {
         Scanner scan = new Scanner(System.in);
-
         System.out.print("May I have your name? ");
         String usrName = scan.next();
         System.out.println("Hello, " + usrName + "!");
