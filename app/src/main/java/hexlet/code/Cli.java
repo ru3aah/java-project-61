5b6c764a5555c4036ad13694f0060ced8a30170f
ru3aah <ru3aah@gmail.com>
2023-02-04 17:23:32 +0300

    add Cli class

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+import java.util.Scanner;
+public class Cli {
+
+    public static String Greeting() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("May I have your name? ");
+        String usrName = scan.next();
+        System.out.println("Hello, " + usrName + "!");
+        return usrName;
+    }
+
+}
