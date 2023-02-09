4f7c00c0a6dec8da0f3620b8e070027537384973
ru3aah <ru3aah@gmail.com>
2023-02-09 12:13:25 +0300

    corrected errors in Class CalcGame.Equation and related methods. Now it works

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -3,10 +3,10 @@ package hexlet.code;
 import hexlet.code.games.CalcGame;
 import hexlet.code.games.EvenGame;
 
-
 import java.util.Objects;
 import java.util.Scanner;
 
+import static hexlet.code.games.CalcGame.Equation.*;
 import static java.lang.System.in;
 
 
@@ -14,8 +14,7 @@ public class Engine {
 
     public static void gameEngine(int gameNum, String usrName) {
 
-        //Greeting msg
-
+        //Display Greeting msg
         switch (gameNum) {
             case (1) -> //Greeting
             {
@@ -27,12 +26,12 @@ public class Engine {
             case (3) -> //CalcGame
                     CalcGame.greetMsg();
         }
-
         //make turns
         String question = null; // a string to be printed out after "Question: "
         String corAns =  null; // correct answer
-        String usrAns;         // user's answer
+        String usrAns;         // user's input
         Scanner ans = new Scanner(in);
+
         for(int i = 0; i <=2; i++) {
             //generate random question and define a correctAnswer
             switch (gameNum) {
@@ -42,9 +41,13 @@ public class Engine {
                     question = String.valueOf(rnd);
                 }
                 case (3) -> { //CalcGame
-                    int res = CalcGame.getRnd().res;
+                    CalcGame.Equation rnd = CalcGame.getRnd();  //Generate new random equation
+                    int op1 = getOp1(rnd);
+                    int op2 = CalcGame.Equation.getOp2(rnd);
+                    int res = CalcGame.Equation.getRes(rnd);
+                    String opSymbol = CalcGame.Equation.getOpSym(rnd);
                     corAns = String.valueOf(res);
-                    question = CalcGame.getRnd().op1 + CalcGame.getRnd().opSymbol + CalcGame.getRnd().op2;
+                    question = op1 + opSymbol + op2;
                 }
             }
             //ask a question
@@ -63,9 +66,11 @@ public class Engine {
                 System.out.println("Let's try again, " + usrName);
                 return;
             }
+            //if not kicked out than then usrAnsw is correct - congrat and make next turn
                 System.out.println("Correct!");
                 System.out.println();
-        }    //true - once again
+        }
+        // all turns successfull - congrat and get out
         System.out.println("Congratulations, " + usrName);
     }
 }
--- app/src/main/java/hexlet/code/games/CalcGame.java
+++ app/src/main/java/hexlet/code/games/CalcGame.java
@@ -7,32 +7,47 @@ import static java.lang.System.out;
 
 public class CalcGame {
 
+    // define a class for equation components
     public static class Equation {
-        public int op1;
-        public int op2;
-        public int res;
-        public String opSymbol;
+        final private int op1; //1st operand
+        final private int op2; //2nd operand
+        final private int res; //operation result
+        final private String opSymbol; // operator
 
         public Equation (int op1, int op2, int res, String opSymbol) {
             this.op1 = op1;
             this.op2 = op2;
-            this.opSymbol = opSymbol;
             this.res = res;
+            this.opSymbol = opSymbol;
+        }
+        //Define getters for Equation class
+        public static int getOp1(Equation equation){
+            return equation.op1;
+        }
+        public static int getOp2(Equation equation) {
+            return equation.op2;
+        }
+        public static int getRes(Equation equation) {
+            return equation.res;
+        }
+        public static String getOpSym(Equation equation) {
+            return equation.opSymbol;
         }
     }
-
+//Greeting for Calculator Game
     public static void greetMsg() {
         out.println("What is the result of the expression?");
     }
 
+    //get new equation using random generator
     public static Equation getRnd() {
-
         Random random = new Random();
         int op1 = random.nextInt(12);        //first operand
         int op2 = random.nextInt(12);        //second operand
         int operator = random.nextInt(3);    //operator
         int res = 0;                               //result
         String operatorSym = null;                 //operator Symbol
+        //define operator
         switch (operator) {
             case (0) -> {
                 operatorSym = "+";
@@ -42,7 +57,11 @@ public class CalcGame {
                 operatorSym = "-";
                 res = op1 - op2;
             }
-            case (2) -> operatorSym = "*";
+            case (2) -> {
+                operatorSym = "*";
+                res = op1 * op2;
+            }
+
         }
         return new Equation(op1, op2, res, operatorSym);
     }
