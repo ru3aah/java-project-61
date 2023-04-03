0a915dd2832fa5ee9a4982250f44073305467aba
Alex Telenkov <ru3aah@gmail.com>
2023-04-03 20:47:40 +0300

    moved Equation generator into Equation class

--- app/src/main/java/hexlet/code/games/CalcGame.java
+++ app/src/main/java/hexlet/code/games/CalcGame.java
@@ -2,18 +2,16 @@ package hexlet.code.games;
 
 import hexlet.code.Engine;
 import hexlet.code.QuestionAnswer;
-
 import java.util.Random;
-
 import static hexlet.code.Engine.ROUND_QTY;
-import static java.lang.System.out;
+
 public class CalcGame {
 
     public static final int RND_BOUND_CALC = 12; //upper bound for RND to avoid counting billions
-    public static final int SIGN_BOUND_CALC = 3; //upper bound for RND for  op sign
+    public static final int SIGN_BOUND_CALC = 3; //upper bound for RND for op sign
     // define a class for equation components
     public static class Equation {
-       private final int op1;
+        private final int op1;
         private final int op2;
         private final int res;
         private final String opSymbol;
@@ -24,6 +22,11 @@ public class CalcGame {
             this.res = ress;
             this.opSymbol = opSymboll;
         }
+
+        public Equation();
+
+        this.op1 = 
+
         //Define getters for Equation class
         public static int getOp1(Equation equation) {
             return equation.op1;
@@ -37,45 +40,47 @@ public class CalcGame {
         public static String getOpSym(Equation equation) {
             return equation.opSymbol;
         }
+
+        //  get new equation using random generator
+        public static Equation getRnd() {
+            Random random = new Random();
+            int op1 = random.nextInt(RND_BOUND_CALC);
+            int op2 = random.nextInt(RND_BOUND_CALC);
+            int operator = random.nextInt(SIGN_BOUND_CALC);
+            int res;
+            String operatorSym;
+            //define operator
+            switch (operator) {
+                case (0) -> {
+                    operatorSym = " + ";
+                    res = op1 + op2;
+                }
+                case (1) -> {
+                    operatorSym = " - ";
+                    res = op1 - op2;
+                }
+                case (2) -> {
+                    operatorSym = " * ";
+                    res = op1 * op2;
+                }
+                default -> {
+                    operatorSym = " M ";
+                    res = 0;
+                }
+            }
+            return new Equation(op1, op2, res, operatorSym);
+        }
+
     }
-//Greeting for Calculator Game
+
     public static String greetMsg() {
         return "What is the result of the expression?";
     }
 
-    //get new equation using random generator
-    public static Equation getRnd() {
-        Random random = new Random();
-        int op1 = random.nextInt(RND_BOUND_CALC);
-        int op2 = random.nextInt(RND_BOUND_CALC);
-        int operator = random.nextInt(SIGN_BOUND_CALC);
-        int res;
-        String operatorSym;
-        //define operator
-        switch (operator) {
-            case (0) -> {
-                operatorSym = " + ";
-                res = op1 + op2;
-            }
-            case (1) -> {
-                operatorSym = " - ";
-                res = op1 - op2;
-            }
-            case (2) -> {
-                operatorSym = " * ";
-                res = op1 * op2;
-            }
-            default -> {
-                operatorSym = " M ";
-                res = 0;
-            }
-        }
-        return new Equation(op1, op2, res, operatorSym);
-    }
     public static void game() {
         QuestionAnswer[] questionAnswer = new QuestionAnswer[ROUND_QTY];
         for (int z = 0; z < ROUND_QTY; z++) {
-            CalcGame.Equation rnd = CalcGame.getRnd();
+            CalcGame.Equation rnd = CalcGame.Equation.getRnd();
             int op1 = CalcGame.Equation.getOp1(rnd);
             int op2 = CalcGame.Equation.getOp2(rnd);
             int res = CalcGame.Equation.getRes(rnd);
